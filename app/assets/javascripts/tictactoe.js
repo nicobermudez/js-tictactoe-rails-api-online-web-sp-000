118154289a0993a5e9b843000eef1f4d7d082109
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-15 15:40:53 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -7,7 +7,7 @@ function buttonListeners() {
       doTurn(this)
     }
   })
-  
+
   $("#save").on('click', () => saveGame())
   $('#previous').on('click', () => showPreviousGames())
   $('#clear').on("click", () => resetBoard())
